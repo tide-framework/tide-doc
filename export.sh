a17dc9cfad3c114495f5b00672e3a5bb5b4e3a92
Nicolas Petton <petton.nicolas@gmail.com>
2014-06-13 14:21:44 +0200

    export in markdown too

--- export.sh
+++ export.sh
@@ -2,4 +2,5 @@
 
 ./pillar export --to='html' 'documentation.pier' > documentation.html
 ./pillar export --to='latex' 'documentation.pier' > documentation.tex
+./pillar export --to='markdown' 'documentation.pier' > readme.md
 
